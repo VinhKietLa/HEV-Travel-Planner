ba0a087eae16661c9d822f82995e24f601502d02
helena <helena.gilja@gmail.com>
2023-03-15 21:22:45 +0000

    rendering data from the api for 6 main categories, tested with buttons, only logging into console

--- vanillaJS_apiTesting/script.js
+++ vanillaJS_apiTesting/script.js
@@ -1,9 +1,12 @@
+let optionsBtn = document.getElementById("checkMyOptions");
+
 // below user input has to be lat and lon
 // get them to pick a location on google maps from google api and get lat and lon values.
 // open trip map api does not work with city names only as it is not specific enough
 // Specify the single latitude and longitude value
 const latitude = 35.7865;
 const longitude = -9.4194;
+let lonMin, lonMax;
 
 // Use the OpenCage Geocoder API to get the location information for the latitude and longitude
 const apiKey = "d4fa4867fc804ba4a8af0e7e9346c89c";
@@ -22,10 +25,10 @@ fetch(url)
         const lon2 = data2.results[0].geometry.lng;
 
         // Calculate the latitude and longitude range for the original location
-        const latMin = Math.min(latitude, lat2);
-        const latMax = Math.max(latitude, lat2);
-        const lonMin = Math.min(longitude, lon2);
-        const lonMax = Math.max(longitude, lon2);
+        latMin = Math.min(latitude, lat2);
+        latMax = Math.max(latitude, lat2);
+        lonMin = Math.min(longitude, lon2);
+        lonMax = Math.max(longitude, lon2);
 
         console.log(`Latitude Range: ${latMin} to ${latMax}`);
         console.log(`Longitude Range: ${lonMin} to ${lonMax}`);
@@ -35,12 +38,72 @@ fetch(url)
         // do we want to specify the sources or give all 4? this increases conditionals but again makes the app more cool
         // default response is 50 results, we can limit it this depending on our need
 
-        const url3 = ` https://api.opentripmap.com/0.1/en/places/bbox?lon_min=${lonMin}&lon_max=${lonMax}&lat_min=${latMin}&lat_max=${latMax}&src_geom=wikidata&rate=1&format=json&limit=20&apikey=5ae2e3f221c38a28845f05b6165ca5f078a6cd7e01751064ebf17758`;
+        const url3 = ` https://api.opentripmap.com/0.1/en/places/bbox?lon_min=${lonMin}&lon_max=${lonMax}&lat_min=${latMin}&lat_max=${latMax}&format=json&limit=200&apikey=5ae2e3f221c38a28845f05b6165ca5f078a6cd7e01751064ebf17758`;
 
         fetch(url3)
           .then((response3) => response3.json())
           .then((data3) => {
             console.log("last", data3);
+            const kindsSet = [];
+
+            // loop through json and get to kinds
+            for (let i = 0; i < data3.length; i++) {
+              // console.log(data3[i].kinds);
+              const kinds = data3[i].kinds.split(",");
+              console.log(kinds);
+              // if(kinds.includes('br'))
+
+              // else if
+              // else if
+              // kindsSet.push(kinds);
+            }
+            console.log(kindsSet);
           });
+
+        // by default all sources are being returned
       });
   });
+
+// write a function to group by kind and render
+
+// subsections: give them all? or perhaps define on separate components of the react app
+
+// when the user clicks on a button give them multiple buttons with 4 main kinds: big groups - accommodations, adult, amusements, interesting_places, sport, tourist_facilitites
+// add event listener on the button
+// get kids container from html
+
+const kindsDiv = document.getElementById("kinds-container");
+
+optionsBtn.addEventListener("click", function (data3) {
+  const kinds = [
+    "accomodations",
+    "adult",
+    "amusements",
+    "interesting_places",
+    "sport",
+    "tourist_facilities",
+  ];
+
+  // loop through kinds and render buttons with each category
+
+  for (let i = 0; i < kinds.length; i++) {
+    const kind = kinds[i];
+    let button = document.createElement("button");
+    button.textContent = kind;
+    button.addEventListener("click", function (e) {
+      // then on each button being pressed, depending on the button, fetch the required kind subcategories
+      let kindValue = e.target.textContent;
+
+      // api construct
+
+      const kindsAPI = ` https://api.opentripmap.com/0.1/en/places/bbox?lon_min=${lonMin}&lon_max=${lonMax}&lat_min=${latMin}&lat_max=${latMax}&kinds=${kindValue}&format=json&limit=200&apikey=5ae2e3f221c38a28845f05b6165ca5f078a6cd7e01751064ebf17758`;
+
+      fetch(kindsAPI)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        });
+    });
+    kindsDiv.appendChild(button);
+  }
+});
