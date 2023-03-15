8448304cedda2f70c9850cf9eb4c492a1bb77ea5
Vinh La <kietla92@Vinhs-MacBook-Pro.local>
2023-03-15 22:43:56 +0000

    google maps api, city search function and market to drop to the nearest city

--- VanillaGoogleMaps/index.js
+++ VanillaGoogleMaps/index.js
@@ -1,45 +1,73 @@
 let userLat = document.getElementById('lat');
 let userLng = document.getElementById('lng');
 
-
 let map;
 
 function initMap() {
-
-//This targets the map div and sets the default location that is displayed on the map when it loads.
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 35.67796492606207, lng: 139.77230167581152 },
+    center: { lat: 51.507351, lng: -0.127758 },
     zoom: 8,
   });
 
-
-  // Creates a new marker everytime the user clicks on the map.
   const marker = new google.maps.Marker({
     map: map,
-    visible: false, // Set the marker to be initially hidden
+    visible: false,
   });
 
-  // This event listener listens to the map and returns the lon and lat values on each click.
-  
-  google.maps.event.addListener(map, "click", function (event) {
-    // Get the latitude and longitude of the clicked location
-    const lat = event.latLng.lat();
-    const lng = event.latLng.lng();
-
-    // Set the position of the marker to the clicked location
-    marker.setPosition(event.latLng);
-
-    // Show the marker
-    marker.setVisible(true);
-
-    // TODO: Add code to display the coordinates to the user or perform any other action
-
-    // console.log(lat);
-    // console.log(lng);
-    userLat.textContent = `${lat}`;
-    userLng.textContent = `${lng}`;
-
-  });
+    // Use the geocoding API to look up the nearest city
+    google.maps.event.addListener(map, "click", function (event) {
+      const lat = event.latLng.lat();
+      const lng = event.latLng.lng();
+    
+      // Use the geocoding API to look up the nearest city
+      const geocoder = new google.maps.Geocoder();
+      geocoder.geocode({ location: { lat, lng } }, function (results, status) {
+        if (status === "OK" && results.length > 0) {
+          // Find the nearest city
+          const cityResult = results.find(result => result.types.includes("locality"));
+    
+          if (cityResult) {
+            // Get the city location and address
+            const cityLocation = cityResult.geometry.location;
+            const cityAddress = cityResult.formatted_address;
+            console.log("Nearest city:", cityAddress);
+    
+            // Set the position of the marker to the nearest city location
+            marker.setPosition(cityLocation);
+            marker.setVisible(true);
+    
+            // Update the UI with the latitude, longitude, and nearest city
+            userLat.textContent = `${cityLocation.lat()}`;
+            userLng.textContent = `${cityLocation.lng()}`;
+            // Add the nearest city to the UI
+            userLat.insertAdjacentHTML("afterend", `, ${cityAddress}`);
+          } else {
+            console.error("No city found");
+    
+            // Set the marker to the center of the map if no city is found
+            marker.setPosition(map.getCenter());
+            marker.setVisible(true);
+    
+            // Update the UI with the latitude and longitude
+            userLat.textContent = `${lat}`;
+            userLng.textContent = `${lng}`;
+            // Remove the nearest city from the UI
+            userLat.nextSibling?.remove();
+          }
+        } else {
+          console.error("Geocode failed:", status);
+    
+          // Set the marker to the center of the map if geocoding fails
+          marker.setPosition(map.getCenter());
+          marker.setVisible(true);
+    
+          // Update the UI with the latitude and longitude
+          userLat.textContent = `${lat}`;
+          userLng.textContent = `${lng}`;
+          // Remove the nearest city from the UI
+          userLat.nextSibling?.remove();
+        }
+      });
+});
 }
-
 window.initMap = initMap;
