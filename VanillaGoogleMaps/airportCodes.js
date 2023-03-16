275740cc43c6b43454387303defb7fc0019a99a4
Vinh La <kietla92@Vinhs-MacBook-Pro.local>
2023-03-16 01:42:53 +0000

    working on getting the user selected option

--- VanillaGoogleMaps/airportCodes.js
+++ VanillaGoogleMaps/airportCodes.js
@@ -6,36 +6,36 @@ const lat = 51.5072178;
 const lng = -0.1275862;
 
 //Function for taking the users input of the city they're flying from and on submit giving the user the local airports which they can select where to fly from.
-submitBtn.addEventListener("click", (event) => {
-  event.preventDefault();
+// submitBtn.addEventListener("click", (event) => {
+//   event.preventDefault();
 
-  //Users location
-  let location = userInput.value;
+//   //Users location
+//   let location = userInput.value;
 
-  //location based on the users input of city.
-  let userDestination = location;
-// using Airport AutoComplete API Request
-  let queryURL = `https://airlabs.co/api/v9/suggest?q=${userDestination}&api_key=31fb9a8b-2207-4389-b92f-55f24946700e`;
-  fetch(queryURL)
-    .then((response) => response.json())
-    .then((response) => {
-      console.log(response.response.airports);
+//   //location based on the users input of city.
+//   let userDestination = location;
+// // using Airport AutoComplete API Request
+//   let queryURL = `https://airlabs.co/api/v9/suggest?q=${userDestination}&api_key=31fb9a8b-2207-4389-b92f-55f24946700e`;
+//   fetch(queryURL)
+//     .then((response) => response.json())
+//     .then((response) => {
+//       console.log(response.response.airports);
 
-      //This response returns the nearest airports based on the city name
-      let startingAirport = response.response.airports;
+//       //This response returns the nearest airports based on the city name
+//       let startingAirport = response.response.airports;
 
-      //This maps through the response and returns the airport name, icao code and country code, the icao code will be used as part of the flight prices api.
-      const airportButtonsHTML = startingAirport
-        .map((airport) => {
-          return `<button>${airport.name}, ${airport.icao_code}, ${airport.country_code}</button>`;
-        })
-        .join("");
-      // This renders the airports as buttons on the page so the user can select where they would prefer to fly from.
+//       //This maps through the response and returns the airport name, icao code and country code, the icao code will be used as part of the flight prices api.
+//       const airportButtonsHTML = startingAirport
+//         .map((airport) => {
+//           return `<button>${airport.name}, ${airport.icao_code}, ${airport.country_code}</button>`;
+//         })
+//         .join("");
+//       // This renders the airports as buttons on the page so the user can select where they would prefer to fly from.
 
-      const startingAirportsDiv = document.getElementById("startingAirports");
-      startingAirportsDiv.innerHTML = airportButtonsHTML;
-    });
-});
+//       const startingAirportsDiv = document.getElementById("startingAirports");
+//       startingAirportsDiv.innerHTML = airportButtonsHTML;
+//     });
+// });
 
 // This function listens to the generated airport buttons and will return the selected icao code which is needed for the flights api fetch//
 function handleAirportButtonClick(event) {
@@ -65,10 +65,11 @@ startingAirportsDiv.addEventListener("click", handleAirportButtonClick);
 
 
 
-const cityInput = document.getElementById("cityInput");
-const citySuggestions = document.getElementById("citySuggestions");
+const cityFromInput = document.getElementById("cityFromInput");
+const cityFromSuggestions = document.getElementById("cityFromSuggestions");
+let selectedCity = null; // Initialize selectedCity variable
 
-cityInput.addEventListener("input", (event) => {
+cityFromInput.addEventListener("input", (event) => {
   const input = event.target.value;
 
   // Fetch city suggestions from an API
@@ -78,7 +79,7 @@ cityInput.addEventListener("input", (event) => {
     .then((response) => {
       console.log(response)
       // Clear existing suggestions
-      citySuggestions.innerHTML = "";
+      cityFromSuggestions.innerHTML = "";
 
       // Add new suggestions to the datalist element
       const cities = response.response.cities;
@@ -86,7 +87,42 @@ cityInput.addEventListener("input", (event) => {
         const option = document.createElement("option");
         option.value = city.name;
         option.textContent = city.name;
-        citySuggestions.appendChild(option);
+        cityFromSuggestions.appendChild(option);
+      });
+    });
+});
+
+cityFromSuggestions.addEventListener("change", () => {
+  selectedCity = cityFromSuggestions.value;
+  console.log('Selected city:', selectedCity);
+
+  // Use selected value for the API?
+});
+
+
+
+const cityToInput = document.getElementById("cityToInput");
+const cityToSuggestions = document.getElementById("cityToSuggestions");
+
+cityToInput.addEventListener("input", (event) => {
+  const input = event.target.value;
+
+  // Fetch city suggestions from an API
+  const queryURL = `https://airlabs.co/api/v9/suggest?q=${input}&api_key=31fb9a8b-2207-4389-b92f-55f24946700e`;
+  fetch(queryURL)
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response)
+      // Clear existing suggestions
+      cityToSuggestions.innerHTML = "";
+
+      // Add new suggestions to the datalist element
+      const cities = response.response.cities;
+      cities.forEach((city) => {
+        const option = document.createElement("option");
+        option.value = city.name;
+        option.textContent = city.name;
+        cityToSuggestions.appendChild(option);
       });
     });
 });
@@ -96,8 +132,17 @@ cityInput.addEventListener("input", (event) => {
 
 //we also need dates for when they go and when they come back-----
 
+//FLIGHTS API ACCEPTED PARAMETERS//
 
-
+// departure-airport-iata []
+// arrival-airport-iata []
+// departure-date [] e.g 2023-04-1
+// return-date [] e.g 2023-04-10
+// number-of-adults[] e.g 1 
+// number-of-children [] e.g 0
+// number-of-infants[] e.g 0 
+// cabin-class [] - can be either "Economy", "Business" or "First" or "Premium_Econoy" e.g Economy
+// currency[] - can USD, EUR, GBP e.g USD
 
 
 
